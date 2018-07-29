7cb85f89c27b4d9afb0447bd51bd019300c29646
zhi <xuzhi@bogon.(none)>
2018-07-29 10:44:16 +0800

    0.0.12

--- XZDropDownMenu/WDConst.h
+++ XZDropDownMenu/WDConst.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "UIView+XZExtension.h"
-#import "Masonry.h"
+//#import "Masonry.h"x
 #define XZCOLOR_RGB(r, g, b) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:1]
 #define XZCOLOR_RGBA(r, g, b, a) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:(a)]
 
