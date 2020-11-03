d1db5f2efe6145431be572614e794d8a48233bfd
徐智 <xu_zhi358033194@163.com>
2020-11-03 16:28:12 +0800

    增加cocoapos支持，去除masnory，优化结构

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '10.0'
+
+target 'XZDropDownMenu' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for XZDropDownMenu
+  pod 'Masonry'
+
+end
--- XZDropDownMenu/include/WDConst.h
+++ XZDropDownMenu/include/WDConst.h
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "Masonry.h"
 #import "UIView+XZExtension.h"
+#import <Masonry/Masonry.h>
 #define XZCOLOR_RGB(r, g, b) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:1]
 #define XZCOLOR_RGBA(r, g, b, a) [UIColor colorWithRed:(r/255.0) green:(g/255.0) blue:(b/255.0) alpha:(a)]
 
